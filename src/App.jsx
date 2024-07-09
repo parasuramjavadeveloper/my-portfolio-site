ac4e17b3f2f5a8cd443674f0134bd163b527cdf5
Parasuram Yerramsetty <parasuramyerramsetty@gmail.com>
2024-07-09 11:24:11 +0530

    Using TailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+
 
 function App() {
   const [activeMenu, setActiveMenu] = useState('about');
@@ -9,25 +9,25 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header>
-        <nav>
+    <div className="font-oswald text-lg">
+      <header className="bg-white p-4 text-center">
+        <nav className="flex justify-center mb-5">
           <a 
             href="#writing" 
-            className={activeMenu === 'writing' ? 'active' : ''} 
+            className={`mx-4 p-2 ${activeMenu === 'writing' ? 'text-pink-600' : 'text-black'}`} 
             onClick={() => handleMenuClick('writing')}
           >
             Writing
           </a>
           <a 
             href="#about" 
-            className={activeMenu === 'about' ? 'active' : ''} 
+            className={`mx-4 p-2 ${activeMenu === 'about' ? 'text-pink-600' : 'text-black'}`} 
             onClick={() => handleMenuClick('about')}
           >
             About
           </a>
         </nav>
-        <div className="content">
+        <div className="w-[490.67px] h-[168px] mx-auto flex flex-col justify-center items-center text-center border border-gray-300">
           {activeMenu === 'about' ? (
             <>
               <h1>Hi, I’m PAARASURHAAM</h1>
@@ -41,8 +41,8 @@ function App() {
           )}
         </div>
       </header>
-      <div className="additional-content">
-        <h3>Nothing is more satisfying than creating something from scratch and putting it into the hands of thousands of users.
+      <div className="additional-content p-5 text-center">
+        <h3 className="font-normal">Nothing is more satisfying than creating something from scratch and putting it into the hands of thousands of users.
             That’s why I work as a front-end developer – primarily on web applications projects. 
             I love pushing pixels and really sweat the details, but
             I also love looking at the bigger picture to create architectures that hold up over time.</h3>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
